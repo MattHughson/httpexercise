98eb8fc44c62b81cbbcadec8af7c0bfc294bbb3a
MattHughson! <matt@koodomasson.com>
2019-06-04 00:18:48 +0000

    all done with step3

---
+++ step3/step3.js
@@ -0,0 +1,56 @@
+var https = require('https');   
+
+function getAndPrintHTML (options) {
+    
+    https.get(options, function (response) {
+        
+        var answer = ""
+
+        // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+      
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function (data) {
+          console.log('Chunk Received. Length:', data.length)
+          answer += data;
+        });
+      
+        // the callback is invoked when all of the data has been received
+        // (the `end` of the stream)
+        response.on('end', function() {
+          console.log('Response stream complete.', answer);
+        });
+      
+      });
+  
+  }
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+  
+  
+//     https.get(requestOptions, function (response) {
+//         var answer =""
+
+//         // set encoding of received data to UTF-8
+//         response.setEncoding('utf8');
+      
+//         // the callback is invoked when a `data` chunk is received
+//         response.on('data', function (data) {
+//           console.log('Chunk Received. Length:', data.length)
+//           answer += data;
+//         });
+      
+//         // the callback is invoked when all of the data has been received
+//         // (the `end` of the stream)
+//         response.on('end', function() {
+//           console.log('Response stream complete.', answer);
+//         });
+      
+//       });
+//     /* Add your code here */
+  
+//   }
+  getAndPrintHTML(requestOptions)
